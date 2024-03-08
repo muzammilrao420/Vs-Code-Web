dc1d7319b657c375f2905c14593be002c837d30c
Muzamil Rao <158730658+muzammilrao420@users.noreply.github.com>
2024-03-09 00:56:31 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,39 @@
+// alert("Please ! enter a valid password")
+// alert("hello World")
+
+// var username ="muzamil";
+// console.log(username)
+
+// var num1 ="100";
+// var num2 ="200";
+// var add =num1 +num2
+//  console.log(add);
+
+// var num1=20;
+// var num1=50;
+
+// var result =num1 + num2;
+// console.log(result);
+
+// var num1=10;
+// num1++ ;
+// console.log(num1)
+
+// var age =18
+// if (age =18){
+//     console.log("allow")
+// }else{
+//     console.log("not allow")
+// }
+
+// var num1 ="500";
+// var num2 ="300";
+// var add =num1 +num2
+//  console.log(add);
+
+// var num1=40;
+// var num2=70;
+
+// var result =num1 + num2;
+// console.log(result);
+
